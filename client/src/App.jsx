32ed9adc19082b3ea0911bf3b58cb667f026809b
Berkay-Aykose <berkay_ayk@hotmail.com>
2025-12-10 16:42:13 +0100

    Project Front-End Start

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button} from 'antd';
+import Header from './components/header/Header.jsx';
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
