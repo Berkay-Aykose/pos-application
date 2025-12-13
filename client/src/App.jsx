503214ba406a51d5c967e29a2f7d1bde698908f4
Berkay-Aykose <berkay_ayk@hotmail.com>
2025-12-13 16:51:26 +0100

    Add categories sidebar UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,32 @@
 import { Button} from 'antd';
 import Header from './components/header/Header.jsx';
+import Categories from './components/categories/Categories.jsx';
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+
+      <div className="home px-6 flex justify-between gap-10">
+
+        <div className="categories flex-1 overflow-auto h-[80vh] pb-50">
+          <div>
+            <Categories />
+          </div>
+        </div>
+
+        <div className="products flex-[8]">
+          <div>
+            products
+          </div>
+        </div> 
+
+        <div className="">
+          <div>
+            cart totals
+          </div>
+        </div>
+      </div>
+    </ >
   );
 }
 
---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <ul className='flex gap-4 flex-col text-lg  '>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+        <span> tümü </span>
+      </li>
+
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+        <span> yiyecek </span>
+      </li>
+
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+        <span> içecek </span>
+      </li>
+
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+        <span> içecek </span>
+      </li>
+
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+        <span> içecek </span>
+      </li>
+
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+        <span> içecek </span>
+      </li>
+
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+        <span> içecek </span>
+      </li>
+    </ul>
+  )
+}
+
+export default  Categories
