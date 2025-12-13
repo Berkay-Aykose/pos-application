64da40c203a2bb52bfce24e5daa0b5eb823ab500
Berkay-Aykose <berkay_ayk@hotmail.com>
2025-12-13 17:26:25 +0100

    Add cart totals component with order summary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/header/Header.jsx';
 import Categories from './components/categories/Categories.jsx';
 import Products from './components/products/Products.jsx';
+import CardTotals from './components/card/CardTotals.jsx';
 function App() {
   return (
     <>
@@ -8,7 +9,7 @@ function App() {
 
       <div className="home px-6 flex justify-between gap-10">
 
-        <div className="categories flex-1 overflow-auto h-[80vh] pb-50">
+        <div className="categories overflow-auto h-[80vh] pb-50">
           <div>
             <Categories />
           </div>
@@ -20,11 +21,12 @@ function App() {
           </div>
         </div> 
 
-        <div className="">
+        <div className="card-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
           <div>
-            cart totals
-          </div>
+            <CardTotals /> 
+          </div> 
         </div>
+          
       </div>
     </ >
   );
--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,32 +1,8 @@
 const Categories = () => {
   return (
     <ul className='flex gap-4 flex-col text-lg  '>
-      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
+      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center min-w-[145px] '>
         <span> tümü </span>
-      </li>
-
-      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
-        <span> yiyecek </span>
-      </li>
-
-      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
-        <span> içecek </span>
-      </li>
-
-      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
-        <span> içecek </span>
-      </li>
-
-      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
-        <span> içecek </span>
-      </li>
-
-      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
-        <span> içecek </span>
-      </li>
-
-      <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all mb-4 text-center'>
-        <span> içecek </span>
       </li>
     </ul>
   )
