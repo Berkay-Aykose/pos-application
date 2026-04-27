525dc678b524c4198f137c7cce68a375d201f2b1
Berkay-Aykose <berkay_ayk@hotmail.com>
2026-04-27 21:12:03 +0200

    Invoice printing page frontend done, customer page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CardPage from './pages/CardPage';
 import BillsPage from './pages/BillPage';
+import CustomerPage from './pages/CustomerPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/card" element={<CardPage />} />
         <Route path="/bills" element={<BillsPage />} />
+        <Route path="/customers" element={<CustomerPage />} />
       </Routes> 
 
     </BrowserRouter>
