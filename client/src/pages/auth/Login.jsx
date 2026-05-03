cd3fc62d73a379180d5e15a38f587431ee06ffac
Berkay-Aykose <berkay_ayk@hotmail.com>
2026-05-03 10:03:51 +0200

    Login Page frontend done

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,66 +1,44 @@
-import { Form, Carousel, Input, Button } from "antd";
+import { Button, Carousel, Checkbox, Form, Input } from "antd";
 import { Link } from "react-router-dom";
 import AuthCarousel from "../../components/auth/AuthCarousel";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="h-screen">
-      <div className="flex h-full justify-between">
+      <div className="flex justify-between h-full">
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
-          <h1 className="text-center text-5xl font-bold">LOGO</h1>
+          <h1 className="text-center text-5xl font-bold mb-2">LOGO</h1>
           <Form layout="vertical">
             <Form.Item
-              label="Kullanıcı Adı"
-              name={"username"}
-              rules={[
-                {
-                  required: true,
-                  message: "Kullanıcı adı boş bırakılamaz!",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="Email"
+              label="E-mail"
               name={"email"}
               rules={[
                 {
                   required: true,
-                  message: "Email boş bırakılamaz!",
+                  message: "E-mail Alanı Boş Bırakılamaz!",
                 },
               ]}
             >
               <Input />
             </Form.Item>
-
             <Form.Item
               label="Şifre"
               name={"password"}
               rules={[
                 {
                   required: true,
-                  message: "Şifre boş bırakılamaz!",
+                  message: "Şifre Alanı Boş Bırakılamaz!",
                 },
               ]}
             >
               <Input.Password />
             </Form.Item>
-
-            <Form.Item
-              label="Şifre Tekrar"
-              name={"passwordAgain"}
-              rules={[
-                {
-                  required: true,
-                  message: "Şifre tekrar boş bırakılamaz!",
-                },
-              ]}
-            >
-              <Input.Password />
+            <Form.Item name={"remember"} valuePropName="checked">
+              <div className="flex justify-between items-center">
+                <Checkbox>Remember me</Checkbox>
+                <Link>Forgot Password?</Link>
+              </div>
             </Form.Item>
-
             <Form.Item>
               <Button
                 type="primary"
@@ -68,20 +46,21 @@ const Register = () => {
                 className="w-full"
                 size="large"
               >
-                Kayıt Ol
+                Giriş Yap
               </Button>
             </Form.Item>
           </Form>
-
           <div className="flex justify-center absolute left-0 bottom-10 w-full">
-            Başka bir hesap var mı? <Link to="/login" className="text-blue-600">Giriş yap</Link>{" "}
+            Henüz bir hesabınız yok mu?&nbsp;
+            <Link to="/register" className="text-blue-600">
+              Şimdi kaydol
+            </Link>
           </div>
         </div>
-        
         <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
           <div className="w-full h-full flex items-center">
             <div className="w-full">
-              <Carousel className="!h-full px-6 autoplay">
+              <Carousel className="!h-full px-6" autoplay>
                 <AuthCarousel
                   img="/images/responsive.svg"
                   title="Responsive"
@@ -111,4 +90,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
