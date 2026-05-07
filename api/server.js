85edf50c83bcbefa09965c9aec352c266775a51a
Berkay-Aykose <berkay_ayk@hotmail.com>
2026-05-07 12:31:50 +0200

    mongoDB installation

--- api/server.js
+++ api/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
-
 const port = 5000;
 
+dotenv.config();
+
+const connectDB = async () => { 
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected");}
+  catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
+  connectDB();
   console.log(`Example app listening on port ${port}`);
 });
