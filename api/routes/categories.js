211b36b994843dfa961638073ca10f8a6606fbb9
Berkay-Aykose <berkay_ayk@hotmail.com>
2026-05-21 19:10:01 +0200

    users, products, bills, auth model done

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -7,8 +7,8 @@ router.get("/get-all", async (req, res) => {
   try {
     const categories = await Category.find();
     res.status(200).json(categories);
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
@@ -18,8 +18,8 @@ router.post("/add-category", async (req, res) => {
     const newCategory = new Category(req.body);
     await newCategory.save();
     res.status(200).json("Item has been added to the database");
-  } catch (err) {
-    res.status(400).json(err);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
@@ -29,7 +29,7 @@ router.put("/update-category", async (req, res) => {
     await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body);
     res.status(200).json("Item updated successfully.");
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
   }
 });
 
@@ -39,7 +39,7 @@ router.delete("/delete-category", async (req, res) => {
     await Category.findOneAndDelete({ _id: req.body.categoryId });
     res.status(200).json("Item deleted successfully.");
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
   }
 });
 
--- api/server.js
+++ api/server.js
@@ -7,6 +7,10 @@ const port = 5000;
 
 // Import routes
 const categoryRoute = require("./routes/categories.js");
+const productRoute = require("./routes/products.js");
+const billRoute = require("./routes/bills.js");
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/users.js");
 
 dotenv.config();
 
@@ -22,7 +26,11 @@ const connectDB = async () => {
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", categoryRoute);
+app.use("/api/categories", categoryRoute);
+app.use("/api/products", productRoute);
+app.use("/api/bills", billRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
   connectDB();
