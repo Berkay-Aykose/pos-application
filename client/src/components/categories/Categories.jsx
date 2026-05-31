1e74657d64de85933035033e55a8726579db16ed
Berkay-Aykose <berkay_ayk@hotmail.com>
2026-05-31 21:56:55 +0200

    category GET done

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -3,7 +3,7 @@ import { PlusOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import { Modal, Form, Input, message } from "antd";
 
-const Categories = () => {
+const Categories = ({ categories, setCategories }) => {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [form] = Form.useForm();
 
@@ -16,21 +16,20 @@ const Categories = () => {
       });
       message.success("Kategori başarıyla eklendi.");
       form.resetFields();
+      setCategories([...categories, values]);
     } catch (error) {
       console.log(error);
     }
   };
+
   return (
     <ul className="flex gap-4 md:flex-col text-lg">
-      <li className="category-item">
-        <span> tümü </span>
-      </li>
-      <li className="category-item">
-        <span> tümü </span>
-      </li>
-      <li className="category-item">
-        <span> tümü </span>
-      </li>
+      {categories.map((item) => (
+        <li className="category-item" key={item._id}>
+          <span>{item.title}</span>
+        </li>
+      ))}
+      
       <li className="category-item !bg-purple-800 hover:opacity-90" onClick={() => setIsAddModalOpen(true)}>
         <PlusOutlined className="md:text-2xl" />
       </li>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,8 +2,26 @@ import Header from '../components/header/Header.jsx';
 import Categories from '../components/categories/Categories.jsx';
 import Products from '../components/products/Products.jsx';
 import CardTotals from '../components/card/CardTotals.jsx';
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/categories/get-all");
+        const data = await res.json();
+        setCategories(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getCategories();
+  }, []);
+
   return (
     <>
       <Header />
@@ -11,7 +29,7 @@ const HomePage = () => {
       <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
         <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
           <div>
-            <Categories />
+            <Categories categories={categories} setCategories={setCategories}/>
           </div>
         </div>
 
